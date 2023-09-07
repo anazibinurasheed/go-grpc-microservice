8f0b2b3218c26e028c59f585b344adfd39f609b9
anazibinurasheed <anazibinurasheed@gmail.com>
2023-09-07 10:49:58 +0530

    grpc-api-gateway added configs and partial program

---
+++ go-grpc-api-gateway/pkg/auth/routes/login.go
@@ -0,0 +1,32 @@
+package routes
+
+import (
+	"context"
+	"github.com/anazibinurasheed/go-grpc-microservice/go-grpc-api-gateway/pkg/auth/pb"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type LoginRequestBody struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func Login(ctx *gin.Context, c pb.AuthServiceClient) {
+	b := LoginRequestBody{}
+	if err := ctx.BindJSON(&b); err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	res, err := c.Login(context.Background(), &pb.LoginRequest{
+		Email:    b.Email,
+		Password: b.Password,
+	})
+
+	if err != nil {
+		ctx.AbortWithError(http.StatusBadGateway, err)
+		return
+	}
+	ctx.JSON(http.StatusCreated, &res)
+}
