af487cb8f431189221e2ff62dd1860547441de97
anazibinurasheed <anazibinurasheed@gmail.com>
2023-09-10 21:46:27 +0530

    Application ready to run.

---
+++ go-grpc-product-svc/pkg/db/db.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"log"
+
+	"gihub.com/anazibinurasheed/go-grpc-microservice/go-grpc-product-svc/pkg/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Handler struct {
+	DB *gorm.DB
+}
+
+func Init(url string) Handler {
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	db.AutoMigrate(
+		&models.Product{},
+		&models.StockDecreaseLog{},
+	)
+
+	return Handler{DB: db}
+
+}
